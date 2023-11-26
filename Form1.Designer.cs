7cc6abee4778890336c4da9df101478b43ecfa66
Nathan Bertaud <nathan.bertaud@ynov.com>
2023-11-26 09:17:18 +0100

    add plex ip + json config + look for ip complection #1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,15 @@
             label1 = new Label();
             label2 = new Label();
             panel1 = new Panel();
+            label3 = new Label();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            label5 = new Label();
+            checkBox1 = new CheckBox();
+            radioButton1 = new RadioButton();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            flowLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
@@ -74,18 +83,103 @@
             // 
             // panel1
             // 
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(843, 57);
-            panel1.TabIndex = 4;
             panel1.BackColor = Color.Red;
             panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(841, 57);
+            panel1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(417, 143);
+            label3.Name = "label3";
+            label3.Size = new Size(44, 15);
+            label3.TabIndex = 8;
+            label3.Text = "default";
+            label3.Visible = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(77, 144);
+            label4.Name = "label4";
+            label4.Size = new Size(100, 15);
+            label4.TabIndex = 7;
+            label4.Text = "ip du serveur Plex";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(187, 140);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(313, 140);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "valider";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(77, 170);
+            label5.Name = "label5";
+            label5.Size = new Size(154, 15);
+            label5.TabIndex = 9;
+            label5.Text = "Chercher automatiquement";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(239, 169);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(83, 19);
+            checkBox1.TabIndex = 10;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(3, 3);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(62, 19);
+            radioButton1.TabIndex = 11;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "default";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.Visible = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoSize = true;
+            flowLayoutPanel1.BackColor = Color.Blue;
+            flowLayoutPanel1.Controls.Add(radioButton1);
+            flowLayoutPanel1.Location = new Point(77, 194);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(200, 161);
+            flowLayoutPanel1.TabIndex = 13;
+            flowLayoutPanel1.Visible = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(841, 470);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(checkBox1);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(button2);
             Controls.Add(panel1);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -93,6 +187,8 @@
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -104,5 +200,13 @@
         private Label label1;
         private Label label2;
         private Panel panel1;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox2;
+        private Button button2;
+        private Label label5;
+        private CheckBox checkBox1;
+        private RadioButton radioButton1;
+        private FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,24 @@
+using System.IO;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 using System.Windows.Forms.Design;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System.Net;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace config_app
 {
     public partial class Form1 : Form
     {
+        public Dictionary<string, string> Json { get; set; }
         public Form1()
         {
+
             InitializeComponent();
             button1.Text = "Valider";
             label1.Text = "IP de la seedbox";
+            Json = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText("../../../config.json"));
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -29,8 +39,40 @@ namespace config_app
                 label2.Text = "default";
                 label2.ForeColor = Color.Black;
                 label2.Visible = false;
+                Json["seedboxIP"] = textBox1.Text;
+                File.WriteAllText("../../../config.json", JsonConvert.SerializeObject(Json));
             }
+        }
+
+        private void checkedRadio()
+        {
 
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            switch (checkBox1.Checked)
+            {
+                case true:
+                    string hostName = Dns.GetHostName();
+                    IPAddress[] IPs = Dns.GetHostEntry(hostName).AddressList;
+                    var i = 2;
+                    label1.Text = IPs[0].ToString();
+                    foreach (var ip in IPs)
+                    {
+                        flowLayoutPanel1.Visible = true;
+                        var radio = new RadioButton();
+                        radio.Text = ip.ToString();
+                        //radio.Location = new Point(panel2.Location.X, panel2.Location.Y + i);
+                        flowLayoutPanel1.Controls.Add(radio);
+                        i += 2;
+                    }
+                    break;
+                case false:
+                    break;
+            }
+        }
+
+
     }
 }
