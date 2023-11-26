fd51f4d01590e8a5d4e5c6a05def11712a0fb739
Nathan Bertaud <nathan.bertaud@ynov.com>
2023-11-26 18:34:38 +0100

    about page + assets +json config#2 + look for ip completion #end

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace config_app
+﻿using System.Reflection.PortableExecutable;
+
+namespace config_app
 {
     partial class Form1
     {
@@ -28,11 +30,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             button1 = new Button();
             textBox1 = new TextBox();
             label1 = new Label();
             label2 = new Label();
             panel1 = new Panel();
+            label6 = new Label();
+            button3 = new Button();
             label3 = new Label();
             label4 = new Label();
             textBox2 = new TextBox();
@@ -41,12 +46,19 @@
             checkBox1 = new CheckBox();
             radioButton1 = new RadioButton();
             flowLayoutPanel1 = new FlowLayoutPanel();
+            panel2 = new Panel();
+            button5 = new Button();
+            button4 = new Button();
+            label7 = new Label();
+            textBox3 = new TextBox();
+            panel1.SuspendLayout();
             flowLayoutPanel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(313, 71);
+            button1.Location = new Point(336, 80);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 0;
@@ -56,16 +68,15 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(187, 71);
+            textBox1.Location = new Point(210, 80);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(100, 23);
             textBox1.TabIndex = 1;
-            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(77, 75);
+            label1.Location = new Point(100, 84);
             label1.Name = "label1";
             label1.Size = new Size(95, 15);
             label1.TabIndex = 2;
@@ -74,7 +85,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(417, 76);
+            label2.Location = new Point(440, 85);
             label2.Name = "label2";
             label2.Size = new Size(44, 15);
             label2.TabIndex = 3;
@@ -83,17 +94,40 @@
             // 
             // panel1
             // 
-            panel1.BackColor = Color.Red;
+            panel1.BackColor = Color.FromArgb(85, 85, 92);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(button3);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
             panel1.Size = new Size(841, 57);
             panel1.TabIndex = 4;
             // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(376, 21);
+            label6.Name = "label6";
+            label6.Size = new Size(116, 15);
+            label6.TabIndex = 1;
+            label6.Text = "Configuration de l'IP";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(85, 85, 92);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(3, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(61, 51);
+            button3.TabIndex = 0;
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(417, 143);
+            label3.Location = new Point(440, 152);
             label3.Name = "label3";
             label3.Size = new Size(44, 15);
             label3.TabIndex = 8;
@@ -103,7 +137,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(77, 144);
+            label4.Location = new Point(100, 153);
             label4.Name = "label4";
             label4.Size = new Size(100, 15);
             label4.TabIndex = 7;
@@ -111,24 +145,25 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(187, 140);
+            textBox2.Location = new Point(210, 149);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(100, 23);
             textBox2.TabIndex = 6;
             // 
             // button2
             // 
-            button2.Location = new Point(313, 140);
+            button2.Location = new Point(336, 149);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 5;
             button2.Text = "valider";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(77, 170);
+            label5.Location = new Point(100, 179);
             label5.Name = "label5";
             label5.Size = new Size(154, 15);
             label5.TabIndex = 9;
@@ -137,7 +172,7 @@
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(239, 169);
+            checkBox1.Location = new Point(262, 178);
             checkBox1.Name = "checkBox1";
             checkBox1.Size = new Size(83, 19);
             checkBox1.TabIndex = 10;
@@ -160,19 +195,82 @@
             // flowLayoutPanel1
             // 
             flowLayoutPanel1.AutoSize = true;
-            flowLayoutPanel1.BackColor = Color.Blue;
+            flowLayoutPanel1.BackColor = Color.FromArgb(85, 85, 92);
             flowLayoutPanel1.Controls.Add(radioButton1);
-            flowLayoutPanel1.Location = new Point(77, 194);
+            flowLayoutPanel1.Location = new Point(100, 203);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(200, 161);
+            flowLayoutPanel1.Size = new Size(245, 161);
             flowLayoutPanel1.TabIndex = 13;
             flowLayoutPanel1.Visible = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(85, 85, 92);
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(button4);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 57);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(84, 413);
+            panel2.TabIndex = 14;
+            panel2.Visible = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(85, 85, 92);
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Location = new Point(3, 67);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 1;
+            button5.Text = "A propos";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(85, 85, 92);
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(3, 19);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 0;
+            button4.Text = "Config";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(96, 446);
+            label7.Name = "label7";
+            label7.Size = new Size(328, 15);
+            label7.TabIndex = 16;
+            label7.Text = "Par Suckiel Theo, Leroi Remi, Lafon Rafael et Bertaud Nathan.";
+            label7.Visible = false;
+            // 
+            // textBox3
+            // 
+            textBox3.Enabled = false;
+            textBox3.ForeColor = Color.Black;
+            textBox3.Location = new Point(277, 170);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(306, 140);
+            textBox3.TabIndex = 17;
+            textBox3.Text = "Cette application permet de rentrer les differentes ip des machines pour eviter d'avoir a les changer dans les fichiers bash.";
+            textBox3.Visible = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(841, 470);
+            Controls.Add(textBox3);
+            Controls.Add(label7);
+            Controls.Add(panel2);
             Controls.Add(flowLayoutPanel1);
             Controls.Add(checkBox1);
             Controls.Add(label5);
@@ -185,10 +283,14 @@
             Controls.Add(label1);
             Controls.Add(textBox1);
             Controls.Add(button1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
-            Text = "Form1";
+            Text = "Configuration app";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             flowLayoutPanel1.ResumeLayout(false);
             flowLayoutPanel1.PerformLayout();
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -208,5 +310,12 @@
         private CheckBox checkBox1;
         private RadioButton radioButton1;
         private FlowLayoutPanel flowLayoutPanel1;
+        private Label label6;
+        private Button button3;
+        private Panel panel2;
+        private Button button5;
+        private Button button4;
+        private Label label7;
+        private TextBox textBox3;
     }
 }
